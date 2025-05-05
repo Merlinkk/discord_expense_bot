81a46d7658d1f1d0838e6de79e92f21bfa5a7692
Merlinkk <anshulkashyap2504@gmail.com>
2025-05-05 12:11:58 +0530

    added port to keep render alive

--- src/index.js
+++ src/index.js
@@ -78,3 +78,11 @@ client.on('interactionCreate', async interaction => {
 
 // Login the bot
 client.login(config.botToken);
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
